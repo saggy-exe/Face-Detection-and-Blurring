021f7fc7da9ed8978e1ff42ed2f4d18da792c173
Sagnic Ghosh <ghoshsagnic04@gmail.com>
2025-01-26 07:23:31 +0530

    blurring face and storing that image or video

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import cv2
 import mediapipe as mp
 
 def process_img(img, face_detection):
-    H, W, _ = img.shape
+    H, W, _= img.shape
     img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     out = face_detection.process(img_rgb)
 
@@ -44,9 +44,6 @@ with mp_face_detection.FaceDetection(model_selection=0, min_detection_confidence
 
     if args.mode in ["image"]:
         img = cv2.imread(args.filePath)
-
-        H, W, _ = img.shape
-
         img = process_img(img, face_detection)
 
         #save image
